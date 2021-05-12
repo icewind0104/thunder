f4bedeecc64ac2b422bb136c5dad0f7f35693862
icewind0104 <icewind0104@163.com>
2021-05-12 11:26:25 +0800

    Signed-off-by: icewind0104 <icewind0104@163.com>

--- expect.sh
+++ expect.sh
@@ -12,7 +12,7 @@ done
 if [ "`cat /nfs/mount | grep ^$1 | wc -l`" == "1" ];then
 	MODEL=`cat /nfs/mount | grep ^$1 | awk '{print $4}'`
 	python3 ./jinja.py $MODEL> /tmp/.service.sh
-	.expect.exp $1
+	./.expect.exp $1
 else
 	echo "Can not find host($1)'s settings."
 fi
