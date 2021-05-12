229cfc29c5d9f958029a3e66ec8837863169aa70
icewind0104 <icewind0104@163.com>
2021-05-12 20:39:18 +0800

    123

--- jinja.py
+++ jinja.py
@@ -1,7 +1,7 @@
-import os, sys
+import os, sys, time
 from jinja2 import Environment, FileSystemLoader
 
 dirname, filename = os.path.split(os.path.abspath(__file__))
 env = Environment(loader=FileSystemLoader(dirname))
 template = env.get_template('src/' + sys.argv[1] + '.sh')
-print(template.render())
+print(template.render(date=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
